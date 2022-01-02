8b97124b7f06f00ffe6e4996ce3d86cfb714d8c6
jhoy-id <89542179+jhoy-id@users.noreply.github.com>
2022-01-02 19:24:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,16 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
+IZIN=$( curl http://akses.netnot.xyz:81/liuuufeydevmain | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
