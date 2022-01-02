804ccac4822bbbb709571ba7f927c5df5f40ab23
Bauaha <89596595+ajidanang@users.noreply.github.com>
2022-01-02 20:33:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/jhoy-id/sc-noip/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/ajidanang/sc-noipe/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/jhoy-id/sc-noipe/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/jhoy-id/sc-noip/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
